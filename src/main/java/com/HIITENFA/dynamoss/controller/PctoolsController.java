6c9bc8a4a74b30f4d2093d35e815f41930ab8d85
Ihab Fallahy <70024187+ihabfallahy2@users.noreply.github.com>
2023-06-08 22:53:47 +0200

    Revert "feat: add-cors-header"

--- src/main/java/com/HIITENFA/dynamoss/controller/PctoolsController.java
+++ src/main/java/com/HIITENFA/dynamoss/controller/PctoolsController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -15,23 +15,19 @@ public class PctoolsController {
     private final PctoolsService pctoolsService;
 
     @PostMapping("/pctools")
-    @CrossOrigin(origins = "*")
     public void save(Pctools data){
         pctoolsService.save(data);
     }
 
     @GetMapping("/pctools")
-    @CrossOrigin(origins = "*")
     public List<Pctools> findAll(){
         return pctoolsService.findAll();
     }
 
     @GetMapping("/pctools/cod/{cod}")
-    @CrossOrigin(origins = "*")
     public Pctools findByCod(@PathVariable String cod){return pctoolsService.findByField(cod);}
 
     @DeleteMapping("/pctools/{id}")
-    @CrossOrigin(origins = "*")
     public void deleteById(@PathVariable String id){
         pctoolsService.deleteById(id);
     }
